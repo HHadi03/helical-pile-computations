09f2aeb9ebd8464a8dfe2979851837a542f08ca7
Hassan Hadi <hhadi6644@gmail.com>
2025-03-31 13:35:47 +0100

    export page fixed

--- app/(sidebar)/export/page.tsx
+++ app/(sidebar)/export/page.tsx
@@ -1,7 +1,19 @@
-export default async function ExportPage(){
+// ServerExportPage.tsx (server component)
+import { getSoils } from '@/lib/getSoils';
+import { getPile } from '@/lib/getPile';
+import { getFactors } from '@/lib/getFactors';
+import Export from './export';
+
+export default async function ServerExportPage() {
+  const soilsData = await getSoils();
+  const pileData = await getPile();
+  const factorsData = await getFactors();
+  
   return (
-    <div>
-      Export Page
-    </div>
-  )
+    <Export
+      initialSoilsData={soilsData} 
+      initialPileData={pileData} 
+      initialFactorsData={factorsData} 
+    />
+  );
 }
